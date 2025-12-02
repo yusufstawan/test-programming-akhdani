aeb2b77efb267cdd60341ec6ed91713ccc591aad
yusufstawan <yusufsetiyawan45@gmail.com>
2025-12-02 16:26:42 +0700

    feat(api, web): init database, prisma, tailwind, shadcn

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
